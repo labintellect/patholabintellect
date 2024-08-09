df39f303dbfc4a05128add9ec71e44055a90786d
Nilay Dhamecha <focuslogics@gmail.com>
2024-08-09 15:34:27 +0530

    Upload initial Project Structure

---
+++ patholabintellect/src/main/java/com/labintellect/patholabintellect/PathoLabIntellectApplication.java
@@ -0,0 +1,13 @@
+package com.labintellect.patholabintellect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathoLabIntellectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathoLabIntellectApplication.class, args);
+	}
+
+}
---
+++ patholabintellect/src/test/java/com/labintellect/patholabintellect/PatholabintellectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labintellect.patholabintellect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatholabintellectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
