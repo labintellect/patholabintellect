f2586975d8b76f09db9c3f729e2441b23b925108
Nilay Dhamecha <focuslogics@gmail.com>
2024-08-10 16:36:47 +0530

    Test commit from new branch with PR

--- src/main/java/com/labintellect/patholabintellect/PatholabintellectApplication.java
+++ src/main/java/com/labintellect/patholabintellect/PatholabintellectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PatholabintellectApplication {
-
+	//start application
 	public static void main(String[] args) {
 		SpringApplication.run(PatholabintellectApplication.class, args);
 	}
