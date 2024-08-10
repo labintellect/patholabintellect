5f097310c2348aca085b8a724ece1029e181c803
Nilay Dhamecha <focuslogics@gmail.com>
2024-08-10 16:22:36 +0530

    Upload initial project structure

---
+++ src/main/java/com/labintellect/patholabintellect/PatholabintellectApplication.java
@@ -0,0 +1,13 @@
+package com.labintellect.patholabintellect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatholabintellectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatholabintellectApplication.class, args);
+	}
+
+}
